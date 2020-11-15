9352ac96a256abe69936b3481914c50f1029fe14
Irina Rogova <buyersbirds@gmail.com>
2020-11-15 18:53:11 +0600

    added file

---
+++ src/test/java/ru/netology/statistics/StatisticsServiceTest.java
@@ -0,0 +1,21 @@
+package ru.netology.statistics;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatisticsServiceTest {
+
+    @org.junit.jupiter.api.Test
+    void findMax() {
+        @Test
+        void findMax() {
+            StatisticsService service = new StatisticsService();
+
+            long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+            long expected = 10;
+
+            long actual = service.findMax(incomesInBillions);
+
+            assertEquals(expected, actual);
+        }
+
+}
